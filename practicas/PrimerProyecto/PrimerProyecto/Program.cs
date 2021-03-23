b3c373d4634b9e3ebcf863901a5a9fe059f8490b
VladiZ <vladizcc@gmail.com>
2021-03-23 19:28:42 -0400

    Iniciando C#

---
+++ practicas/PrimerProyecto/PrimerProyecto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2, b = 3, c = 0;
+            Console.WriteLine("Valores : \n" + a + " " + b);
+            a += b;
+            b -= a;
+            Console.WriteLine("diferente forma de asignacion suma y resta: \n" + a + " " + b);
+            a *= b;
+            a /= b;
+            c = b;
+            c %= b;
+            Console.WriteLine("diferente forma de asignacion multu y div: \n"+ a +" "+ b+" " + c);
+        }
+    }
+}
