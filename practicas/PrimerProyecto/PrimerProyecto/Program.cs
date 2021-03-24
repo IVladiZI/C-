ecc8595892d0a7431597c1fe0aced6ee61ec5051
VladiZ <vladizcc@gmail.com>
2021-03-23 20:09:22 -0400

    Agregacion de comentarios

--- practicas/PrimerProyecto/PrimerProyecto/Program.cs
+++ practicas/PrimerProyecto/PrimerProyecto/Program.cs
@@ -6,16 +6,21 @@ namespace PrimerProyecto
     {
         static void Main(string[] args)
         {
-            int a = 2, b = 3, c = 0;
-            Console.WriteLine("Valores : \n" + a + " " + b);
+            int a = 2, b = 3, c = 1; Console.WriteLine("Valores : \n" + a + " " + b);
+           
             a += b;
             b -= a;
-            Console.WriteLine("diferente forma de asignacion suma y resta: \n" + a + " " + b);
+            //Asignar valores mediante operadores de suma y resta
+            Console.WriteLine("Resultado 1 \n" + a + " " + b);
             a *= b;
             a /= b;
             c = b;
             c %= b;
-            Console.WriteLine("diferente forma de asignacion multu y div: \n"+ a +" "+ b+" " + c);
+            //diferente forma de asignacion multiplicacion ,division y resto
+            Console.WriteLine("Resultado 2: \n"+ a +" "+ b+" " + c);
+            var valorDato = a > b;
+            //diferente forma de asignacion multiplicacion ,division y resto
+            Console.WriteLine("Resultado 3: \n" + valorDato);
         }
     }
 }
