cbc7eea685a4b5f3495ba461879e2e5cabe45846
VladiZ <vladizcc@gmail.com>
2021-09-30 23:33:37 -0400

    Revert "Revert "Cambios de comentarios""
    This reverts commit 09ad8e9a4f3cda7dca958d727a323eb88d41c9f4.

---
+++ TourBolivia_Service/TourBolivia.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TourBolivia.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ practicas/PrimerProyecto/Cadenas/Cadenas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Cadenas
+{
+    public class Cadenas
+    {
+        string valor1 { get; set; }
+        string valor2 { get; set; }
+        int valor3 { get; set; }
+
+        public void eliminaRepetidos(string cadena1)
+        {
+            Dim contador As new IDictionary()();
+        }
+    }
+    public class CadenaYEntero
+    {
+        string cadena1 { get; set; }
+        string cadena2 { get; set; }
+        int entero1 { get; set; }
+        public CadenaYEntero(string va1, string va2, int i1)
+        {
+            this.cadena1 = va1;
+            cadena2 = va2;
+            entero1 = i1;
+        }
+    }
+
+}
